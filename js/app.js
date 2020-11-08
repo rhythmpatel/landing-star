191f0d84f41a6113ee0b51c4fd2a6df8b85f9a2c
Rhythm Ruparelia <rhythm19@gmail.com>
2020-11-08 18:36:27 +0530

    Initial commit - setup css and js structure

---
+++ js/app.js
@@ -0,0 +1,43 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/app"],{
+
+/***/ "./scripts/app.js":
+/*!************************!*\
+  !*** ./scripts/app.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+try {
+  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+
+  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
+} catch (e) {}
+
+/***/ }),
+
+/***/ "./styles/app.scss":
+/*!*************************!*\
+  !*** ./styles/app.scss ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!************************************************!*\
+  !*** multi ./scripts/app.js ./styles/app.scss ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/abc/Projects/sell/landing-star/html/scripts/app.js */"./scripts/app.js");
+module.exports = __webpack_require__(/*! /Users/abc/Projects/sell/landing-star/html/styles/app.scss */"./styles/app.scss");
+
+
+/***/ })
+
+},[[0,"/js/manifest","/js/vendor"]]]);
+//# sourceMappingURL=app.js.map
